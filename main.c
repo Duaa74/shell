82496553454534460c378f04da137661a6f684ae
Monzer7 <monzer-7@hotmail.com>
2023-11-15 12:50:19 -0800

    sfd

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
 #include "main.h"
+/**
+ * main - main function.
+ * @ac: number of arguments
+ * @argv: arguments
+ * @env: environment
+ * Return: number of arguments
+*/
+
 int main(int ac, char **argv, char **env)
 {
-if (ac <= 12) /* changed from 1 to 12*/
-shell(argv,env);
+	if (ac <= 12) /* changed from 1 to 12*/
+	shell(argv, env);
 
-    return (0);
+	return (0);
 }
